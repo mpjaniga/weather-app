c0d64d28ce9e8e8be2142def003473a03c1af929
mpjaniga <melaniejaniga@gmail.com>
2021-10-17 15:58:53 +1100

    JavaScript Update

--- src/app.js
+++ src/app.js
@@ -1,42 +1,19 @@
-let now = new Date();
-let h6 = document.querySelector("#current-date");
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-h6.innerHTML = day + " " + hours + ":" + minutes;
-
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.getElementById("search-text-input").value;
-  document.getElementById("h1-id").innerHTML = searchInput;
-
-  axios
-    .get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&units=metric&appid=${apiKey}`
-    )
-    .then(showTemperature);
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.main;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
 }
-
-function showTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let temperatureElement = document.getElementById("temperature");
-  temperatureElement.innerHTML = `${temperature}°C`;
-}
-
-let button = document.querySelector("#submitButton");
-button.addEventListener("click", search);
 
 let apiKey = "f09d3949047ab6c9e3bcaf79cf61f619";
-let city = "Sydney";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}&units= metric`;
 
-axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
