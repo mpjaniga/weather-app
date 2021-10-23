bf6a4f92695a91aca6ce80f8693c2774bc61396d
mpjaniga <melaniejaniga@gmail.com>
2021-10-23 19:04:55 +1100

    added forecast function

--- src/app.js
+++ src/app.js
@@ -21,6 +21,38 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  let forecastHTML = `<div class="row">`;
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  
+                <div class="col-2">
+                  <div class="weather-forecast-date">${day}</div>
+                  <img
+                    src="http://openweathermap.org/img/wn/04n@2x.png"
+                    alt=""
+                    width="42"
+                  />
+                  <div class="weather-forecast-temp">
+                  
+                  <span class ="weather-forecast-max"> 18° </span>
+                  <span class="weather-forecast-min"> 12° </span>
+                  </div>
+                </div>
+              
+              `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -88,3 +120,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 search("New York");
+
+displayForecast();
